eb521adb0d639790fd2bef051286ca3a959d2f33
richHong <rich.hong@gmail.com>
2016-03-16 12:30:52 -0700

    in middle of queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,36 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  //console.log("storage line 6", storage)
   // Implement the methods below
 
+
+  //Declare two variables one to keep track of size and one to keep track of first in que
+  var count = 0;
+  var reverseCount = 1;
+
+
   someInstance.enqueue = function(value){
+    //increment count variable
+    count++;
+    //set storage key to count and storage property to value
+    storage[count] = value;
+    console.log('this is storage after enqueue', storage)
   };
 
   someInstance.dequeue = function(){
+    console.log("this is storage before dequeue", storage)
+    var removed = storage[reverseCount];
+    delete storage[reverseCount];
+    if(count !== 0){
+      count--
+    }
+    reverseCount++;
+    return removed;
   };
 
   someInstance.size = function(){
+    return count;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,13 +5,24 @@ var Stack = function(){
   var storage = {};
 
   // Implement the methods below
+  var count = 0;
   someInstance.push = function(value){
+    count++;
+    storage[count] = value;
+    console.log(storage[count])
   };
 
   someInstance.pop = function(){
+    var removed = storage[count]
+    delete storage[count];
+    if(count !== 0){
+    count--;
+    }
+    return removed;
   };
-
+console.log('after pop', storage[count])
   someInstance.size = function(){
+    return count;
   };
 
   return someInstance;
