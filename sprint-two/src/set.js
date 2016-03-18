7f78af7738650b837130d74151d6ec1c67cd8838
richHong <rich.hong@gmail.com>
2016-03-18 14:20:59 -0700

    finished set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,12 +7,15 @@ var Set = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this[item] = item;
 };
 
 setPrototype.contains = function(item){
+  return _.contains(this, item)
 };
 
 setPrototype.remove = function(item){
+  delete this[item];
 };
 
 /*
