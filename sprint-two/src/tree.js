e5852c6e07c77776df37804dff775ef718813f6b
richHong <rich.hong@gmail.com>
2016-03-17 17:30:55 -0700

    finished tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,27 +1,41 @@
 var Tree = function(value){
-  var newTree = {};
+  var newTree = Object.create(treeMethods)//{};
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
 
 
   // your code here
-  newTree.children = null;  // fix me
+  //newTree.children = null;  // fix me
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var newBranch = new Tree(value);
+  this.children.push(newBranch);
 };
+
+
 
 treeMethods.contains = function(target){
-
+  var theTruth = false
+  _.each(this.children, function(child){
+    console.log(list)
+    if(child.value === target){
+      theTruth = true;
+    }
+  });
+  return theTruth
 };
+
+//Tree.prototype = treeMethods;
+
+
+
 
 
 /*
