b21e433855566fcdc6d02649ad70179ce203c498
richHong <rich.hong@gmail.com>
2016-03-18 20:36:51 -0700

    finished hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,25 +1,54 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._holder = [];
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i, [k,v]);
+  //var bucket = this._storage.get(i);
+  if (this._holder.length === 0) {
+    this._storage.set(i,[[k,v]]);
+    this._holder.push(k);
+  } else if (_.contains(this._holder, k)) {
+      this._storage.each(function(bucket, index, storage) {
+        if(index === i){
+          _.each(bucket, function(array, index){
+            if (k === array[0]){
+              array[1] = v;
+            }
+          });
+        }
+      });
+  } else {
+    this._storage.each(function(bucket, index, storage){
+      if(index === i) {
+            bucket.push([k,v])
+      }    
+    });
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i)[1];
+  var bucket = this._storage.get(i)
+  var result;
+  _.each(bucket, function(array, index) {
+    if(array[0] === k){
+      result = array[1];
+    }
+  });
+  return result;
 };
 
 HashTable.prototype.remove = function(k){
-  this._storage.each(function(val, index, list){
-    //console.log("val", val, "index", index, "list", list, 'list[index]', list[index]);
-    if(val !== undefined && k === val[0]){
-      val[1] = null;
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i)
+  _.each(bucket, function(array, index) {
+    if(array[0] === k){
+      array[1] = null;
     }
-  })
+  });
 };
 
 
